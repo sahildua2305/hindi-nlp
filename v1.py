a77a8f0b93aeb34c71b1b4558111b3ecca92f630
Sahil <sahildua2305@gmail.com>
2014-08-09 16:38:37 +0530

    v1.py for developing hyponyms using a couple of relation words like 'naamak'&'jaise'

---
+++ v1.py
@@ -0,0 +1,49 @@
+import re
+
+#Initialization of variables
+separators = [u"|"]
+ending_words = []
+decoded_ending_words = []
+hyponyms = []
+words_for_relations=[]
+related_words=[]
+count=0
+count_diff=0
+index=0
+output=''
+#...........................
+
+# Preprocessing
+words_for_relations.append([unichr(i) for i in [2344,2366,2350,2325]]) #naamak
+words_for_relations.append([unichr(i) for i in [2332, 2376, 2360, 2375]]) #jaise
+#.........................
+
+# Read the file containing Hindi text
+file = open('sample.txt', 'r')
+data = file.read().decode('utf-8')
+words = data.split()
+#.................................
+
+for word in words:
+    #print word, len(word),
+    #print [ord(t) for t in word]
+    curr_word=[]
+    for ch in word:
+        curr_word = [unichr(ord(i)) for i in word]
+    if curr_word in words_for_relations:
+        related_words.append([words[index-1], words[index+1]])
+        print words[index-1], words[index+1]
+    
+    # 2404 is the code for '|'. Used for calculating number of sentences in the file
+    if ord(word[-1]) == 2404:
+        count+=1
+        temp_ending_list = [ord(i) for i in word[:-1]]
+        if temp_ending_list not in ending_words:
+            ending_words.append(temp_ending_list)
+            count_diff+=1
+    #............................................
+    index+=1
+
+print "Total number of sentences: "+str(count)
+print "Total different ending words: "+str(count_diff)
+print "Hyponyms found: ", related_words
